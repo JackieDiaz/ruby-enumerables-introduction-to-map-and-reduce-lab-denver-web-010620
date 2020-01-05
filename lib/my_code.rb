3650c3317cb1833810749442e0308f46c8e22f0e
alexander diaz <alexanderjdiaz89@gmail.com>
2020-01-05 23:56:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,66 @@
-# My Code here....
+def map_to_negativize(array)
+new_array = []
+  array.each do |x|
+    if x > 0 
+        x = x * -1
+        new_array << x
+      else x < 0
+        x = x * -1
+        new_array << x
+      end 
+    end 
+    new_array
+    end 
+ def map_to_no_change(array)
+   array
+ end 
+ 
+ def map_to_double(array)
+   
+   new_array = []
+  array.each do |x|
+    if x > 0 
+        x = x * 2
+        new_array << x
+      else x < 0
+        x = x * 2
+        new_array << x
+      end 
+    end 
+    new_array
+    end 
+    
+def map_to_square(array)
+  new_array = []
+  array.each do |x|
+    if x > 0 
+        x = x * x
+        new_array << x
+      else x < 0
+        x = x * x
+        new_array << x
+      end 
+    end 
+    new_array
+    end 
+def reduce_to_total(array)
+i = 0 
+while i < array.length do 
+if i != 0
+  array[i] = array[i] + array[i-1]
+  
+  end 
+  i += 1 
+  end 
+array.last
+end 
+
+
+
+# y = 0
+# array.each do |x|
+# y = x + x 
+# end 
+# y
+# end 
+ 
